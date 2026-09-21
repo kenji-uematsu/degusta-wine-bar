77ffdf72ce8c34ca6b5a3eedd35f2bf3a03779e5
kenji-uematsu <FZF01113@nifty.com>
2026-09-21 20:16:22 +0900

    検索エンジンインデックス有効化

--- src/data/site.ts
+++ src/data/site.ts
@@ -8,5 +8,5 @@ export const site = {
   address, hours: '19:00〜翌5:00', lastEntry: '翌3:00', closedDays: '火曜日',
   access: '新丸子駅から徒歩3分',
   mapUrl: 'https://www.google.com/maps/place/Wine+Shop%26Bar+Degusta/@35.5792294,139.6603336,17z/data=!4m14!1m7!3m6!1s0x6018f55a4bb9836b:0x589533d7317096af!2sWine+Shop%26Bar+Degusta!8m2!3d35.5792294!4d139.6629085!16s%2Fg%2F11x6r_8461!3m5!1s0x6018f55a4bb9836b:0x589533d7317096af!8m2!3d35.5792294!4d139.6629085!16s%2Fg%2F11x6r_8461?entry=ttu&g_ep=EgoyMDI2MDkwMi4wIKXMDSoASAFQAw%3D%3D',
-  preview: true,
+  preview: false,
 };
